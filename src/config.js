3fab3cf5d733920622af2452483b82d93bdecb9f
John F. Mercer <john@radify.io>
2015-08-21 18:08:38 -0400

    Updates packages
    * Also removes two accidentally committed files

--- src/config.js
+++ src/config.js
@@ -20,9 +20,8 @@ System.config({
     "angular": "npm:angular@1.4.0-rc.2",
     "angular-ui-router": "npm:angular-ui-router@0.2.15",
     "babel": "npm:babel-core@5.8.22",
-    "babel-core": "npm:babel-core@5.8.22",
-    "babel-runtime": "npm:babel-runtime@5.2.17",
-    "core-js": "npm:core-js@0.9.8",
+    "babel-runtime": "npm:babel-runtime@5.8.20",
+    "core-js": "npm:core-js@1.1.1",
     "marked": "npm:marked@0.3.3",
     "ramda": "npm:ramda@0.14.0",
     "github:jspm/nodelibs-process@0.1.1": {
@@ -34,8 +33,13 @@ System.config({
     "npm:angular@1.4.0-rc.2": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:core-js@0.9.8": {
+    "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@1.1.1": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "process": "github:jspm/nodelibs-process@0.1.1",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:ramda@0.14.0": {
       "process": "github:jspm/nodelibs-process@0.1.1"
