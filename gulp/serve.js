2e5b1b2a64bf44cd90f4eeeca2ba909f92d4df40
John F. Mercer <john@radify.io>
2015-08-21 19:43:02 -0400

    Refactors gulp setup

---
+++ gulp/serve.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+// Start local dev server.
+gulp.task('serve', function() {
+  connect.server({
+    root: global.paths.index,
+    port: 3003,
+    livereload: false
+  });
+
+  console.log("Demo server started at localhost:3003");
+});
