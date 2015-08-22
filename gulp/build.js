f7c44ad21a09fa0984baf16d349d88a31141828d
John F. Mercer <john@radify.io>
2015-08-21 22:35:50 -0400

    Extracts buildjs task into its own file

--- gulp/build.js
+++ gulp/build.js
@@ -1,21 +1,10 @@
 'use strict';
 
 var gulp   = require('gulp');
-var exec   = require('child_process').execSync;
 var runSeq = require('run-sequence');
 
 gulp.task('build', function(done) {
   runSeq('clean', ['buildjs'], done);
 });
 
-// Build JS for distribution.
-gulp.task('buildjs', function () {
-  exec('npm run buildjs', function (err, stdout, stderr) {
-    if (err) {
-      throw err;
-    }
-    else {
-      console.log('Build complete!');
-    }
-  });
-});
+
