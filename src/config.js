d5a1bb4c968d9b6a34a51b0d8d59434be438cc5c
John F. Mercer <john@radify.io>
2015-08-26 17:10:49 -0400

    Runs tests and adds CI
    * The default task now runs both build and test
    * Travis now runs `gulp` instead of `gulp build`
    * Adds 'test' to package.json scripts
    * Restores jspm's config.js
    * Per jspm docs, adds jspm as npm devDependency
    * Travis now runs npm & jspm locally, not globally
    * TODO: setup PhantomJS testing
      * Currently, the parsers and validators are tested
      * BUT the UI code is NOT tested
      * We must add UI code testing at some point.

---
+++ src/config.js
@@ -0,0 +1,41 @@
+System.config({
+  baseURL: "src/",
+  defaultJSExtensions: true,
+  transpiler: "babel",
+  babelOptions: {
+    "optional": [
+      "runtime"
+    ]
+  },
+  paths: {
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*",
+    "*Spec": "../spec/*.js"
+  },
+
+  map: {
+    "angular": "npm:angular@1.4.0-rc.2",
+    "angular-ui-router": "npm:angular-ui-router@0.2.15",
+    "babel": "npm:babel-core@5.8.22",
+    "babel-core": "npm:babel-core@5.8.22",
+    "babel-runtime": "npm:babel-runtime@5.2.17",
+    "core-js": "npm:core-js@0.9.8",
+    "marked": "npm:marked@0.3.3",
+    "ramda": "npm:ramda@0.14.0",
+    "github:jspm/nodelibs-process@0.1.1": {
+      "process": "npm:process@0.10.1"
+    },
+    "npm:angular-ui-router@0.2.15": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:angular@1.4.0-rc.2": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:core-js@0.9.8": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    },
+    "npm:ramda@0.14.0": {
+      "process": "github:jspm/nodelibs-process@0.1.1"
+    }
+  }
+});
